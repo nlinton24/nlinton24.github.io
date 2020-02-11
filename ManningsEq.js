af0ac13198a38ff32ff1e1eeebf323bc25ca7ded
nlinton24 <60158519+nlinton24@users.noreply.github.com>
2020-02-11 15:01:16 -0700

    q

--- ManningsEq.js
+++ ManningsEq.js
@@ -6,13 +6,17 @@ function myfunc() {
     s = document.getElementById("s").value
     units = document.getElementById("units").value
 
-    if (units = 'SI') {
-        str='m/s'
-        valNum = 1 / n * r ^ (2 / 3) * s ^ (.5) + ' m/s';
+    if (units =='SI') {
+        str=' m/s'
+        valNum =1 / n * r ^ (2 / 3) * s ^ (.5) + ' m/s'
     } else {
-        str='ft/s'
-        valNum = 1.49 / n * r ^ (2 / 3) * s ^ (.5) + 'str';
+        str=' ft/s'
+        valNum =1.49 / n * r ^ (2 / 3) * s ^ (.5) + ' ft/s'
     }
 
     document.getElementById('outResult').innerHTML = valNum;
+    document.getElementById('sout').innerHTML = s
+    document.getElementById('nout').innerHTML = n
+    document.getElementById('unitsout').innerHTML = units
+    document.getElementById('rout').innerHTML = r
 }
